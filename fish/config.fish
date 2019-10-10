ba8e8996c890ad1f62a29724b49b14d4e6e1452e
chux0519 <chuxdesign@hotmail.com>
2019-10-10 21:09:58 +0800

    fix: fish greeting function
    add ccls for c/c++ ls

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,4 @@
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/chux0519/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/chux0519/Downloads/google-cloud-sdk/path.fish.inc'; end
 direnv hook fish | source
-set fish_greeting
-pfetch
+# pfetch
