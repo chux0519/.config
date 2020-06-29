3f09393c019a8f6ce45d6581241f8d30d5ffa1ee
chux0519 <chuxdesign@hotmail.com>
2020-06-29 09:23:55 +0800

    x230: more settings
    - add newsboat configs
    - change bg image
    - disable picom
    - add conda script in fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,9 @@ direnv hook fish | source
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/chux0519/workspace/chux0519/google-cloud-sdk/path.fish.inc' ]; . '/home/chux0519/workspace/chux0519/google-cloud-sdk/path.fish.inc'; end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/chux0519/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
